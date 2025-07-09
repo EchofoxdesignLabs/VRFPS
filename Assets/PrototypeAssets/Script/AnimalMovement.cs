4ac3f52076bbb665e52f8956b4f483d59b0dd6d2
Ykp-echo <yadhu@echofox.design>
2025-07-09 16:58:06 +0530

    Added animated animals

---
+++ Assets/PrototypeAssets/Script/AnimalMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb7fa688f7b49dc052486a5eb342fe14cd6faff6351ab0f582a7574a0e0aa528
+size 4403
---
+++ Assets/ithappy/Animals_FREE/Scripts/PlayerCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c65d7d3a4b10869a9ceb37295c0e26b248cecd84bd05e96985ecfb3c9feefc3
+size 1849
