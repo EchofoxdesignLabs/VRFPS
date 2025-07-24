4579ddf77d88c86513a8cdad793d7629745edc99
Abhijithaabi <abhijithaabi@gamil.com>
2025-07-24 15:24:16 +0530

    Updated Colorformat to ARGB

---
+++ Assets/Art/Rendering/Shader/BenDayDots.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6f7b34857a9f7c6f68cd80d3b7608630a4aee79ca2e8f1f3f151b10b36ab533
+size 3563
---
+++ Assets/Scripts/Rendering/BenDayBloom.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ac18ad5e31444608e6269bb9fa44afe497d38844eecd3551c97c9687906a2f9
+size 10635
