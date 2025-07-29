b59126537e3435955dbba08e467220a345a98e73
Abhijithaabi <abhijithaabi@gamil.com>
2025-07-29 16:31:37 +0530

    Setting global texture directly

--- Assets/Scripts/Rendering/Sketch.cs
+++ Assets/Scripts/Rendering/Sketch.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a9df955252c737e4ceaeb09994d4f8e25dbe02c9d3961a6e565cf4ffb98769d
-size 15120
+oid sha256:0348390c3ecf657ee95c71a67d8e55230eb20400d977251d6bed7f96e02befa1
+size 15288
